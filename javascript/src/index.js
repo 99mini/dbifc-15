4b1e3d98bfcad6c234dd3de9ca0b6df5dbc19f8f
99mini <itman390@gmail.com>
2025-02-19 01:54:51 +0900

    기준일에 못미치는 데이터 수집

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,12 +1,24 @@
 import scraper from "./scraper.js";
-import { removeDuplicatesAndOverWrite } from "./utils.js";
+import {
+  removeDuplicatesAndOverWrite,
+  findNonScrapedProductByDate,
+  findNonScrapedProductByMetaData,
+} from "./utils.js";
 
 const BASE_DATE = "2025-02-01T00:00:00Z";
 
 async function main() {
-  // await scraper.run();
+  await scraper.run();
   // await scraper.test();
   removeDuplicatesAndOverWrite();
+
+  const nonScrapedByMetaData = findNonScrapedProductByMetaData(
+    "product_meta_data2.csv"
+  );
+  console.log("nonScrapedByMetaData", nonScrapedByMetaData);
+
+  const nonScrapedByDate = findNonScrapedProductByDate(BASE_DATE);
+  console.log("nonScrapedByDate", nonScrapedByDate);
 }
 
 main();
